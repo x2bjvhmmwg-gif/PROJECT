758cbe64ca7759492d934de88c2cf5f081b7a9f7
x2bjvhmmwg-gif <as2237as@gmail.com>
2026-04-15 13:05:14 +0900

    포트 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['localhost','quadecologics.cloud'],
+    allowedHosts: ['localhost','aiedu.tplinkdns.com'],
   },
   resolve: {
     alias: {
