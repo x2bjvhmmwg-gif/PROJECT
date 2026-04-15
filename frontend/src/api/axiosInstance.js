51dd8929305750d38683597745b92d5af7b425d2
x2bjvhmmwg-gif <as2237as@gmail.com>
2026-04-15 12:48:23 +0900

    cors 수정

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:8000/api', // 베이스 URL 설정
-  baseURL: 'http://aiedu.tplinkdns.com:8001',
+  baseURL: 'http://aiedu.tplinkdns.com:8001/api',
   headers: {
     'Content-Type': 'application/json',
   },
