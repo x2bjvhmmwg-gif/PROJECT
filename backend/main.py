e13d3b7a721d9872bbec7a6d45e32832b4c63975
x2bjvhmmwg-gif <as2237as@gmail.com>
2026-04-15 13:08:42 +0900

    axios 수정

--- backend/main.py
+++ backend/main.py
@@ -23,4 +23,4 @@ app.add_middleware(
 app.include_router(chat_router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
